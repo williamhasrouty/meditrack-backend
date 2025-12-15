f60b7358a7f54afe379bf539ed858cf910f04847
william.hasrouty <williamhasrouty@yahoo.com>
2025-12-15 13:50:12 -0800

    fix: connect administrations and clients routes
    - Import and mount clients, administrations, and users routers
    - Add auth middleware protection to all protected routes
    - Routes are now accessible at /clients, /administrations, /users

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
 const { createUser, login } = require("../controllers/users");
 const { validateSignup, validateSignin } = require("../middlewares/validation");
+const auth = require("../middlewares/auth");
+const clientsRouter = require("./clients");
+const administrationsRouter = require("./administrations");
+const usersRouter = require("./users");
 
-// Signup route
+// Public routes
 router.post("/signup", validateSignup, createUser);
-
-// Signin route
 router.post("/signin", validateSignin, login);
+
+// Protected routes (require authentication)
+router.use("/clients", auth, clientsRouter);
+router.use("/administrations", auth, administrationsRouter);
+router.use("/users", auth, usersRouter);
 
 module.exports = router;
